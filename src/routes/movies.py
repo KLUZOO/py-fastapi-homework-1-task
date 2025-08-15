7a678a03b3e17c429f9738da710bbf86b3399195
Sasha <kluzodota@gmail.com>
2025-08-15 17:54:37 +0300

    solution

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -35,9 +35,9 @@ async def read_movies(
 
     return MovieListResponseSchema(
         movies=movies,
-        prev_page=f"http://localhost:8000/api/v1/theater/movies?page={page - 1}"
+        prev_page=f"/theater/movies/?page={page - 1}"
                   f"&per_page={per_page}" if page > 1 else None,
-        next_page=f"http://localhost:8000/api/v1/theater/movies?page={page + 1}"
+        next_page=f"/theater/movies/?page={page + 1}"
                   f"&per_page={per_page}" if page < total_pages else None,
         total_pages=total_pages,
         total_items=total_items
